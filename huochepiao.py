e45252e393ed956233f3f3129bc157598377c1be
zs <17621655012@163.com>
2018-01-17 19:40:31 +0800

    add huochepiao.py

---
+++ huochepiao.py
@@ -0,0 +1,75 @@
+
+"""Train tickets query via command-line.
+Usage:
+   tickets [-gdtkz] <上海> <西安> <20180214>
+Options:
+   -h,--help   显示帮助菜单
+   -g          高铁
+   -d          动车
+   -t          特快
+   -k          快速
+   -z          直达
+Example:
+   tickets 上海 西安北 2018-02-14
+"""
+
+from docopt import docopt
+import requests
+import stations
+from prettytable import PrettyTable  #格式化信息打印工具库
+
+class Tickets(object):
+
+    def printTrainInfo(self):
+        arguments = docopt(__doc__)
+        print(arguments['<date>'],arguments['<from>'],arguments['<from>'])
+        date = arguments['<date>']
+        fromstation = stations.getCode(arguments['<from>'])
+        tostation = stations.getCode(arguments['<to>'])
+                
+        url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)
+        r = requests.get(url)
+        print(url)
+        # print(r.json())
+        allresults = r.json()
+        allTickets = allresults['data']['result']
+        # print(len(allTickets),allTickets)
+        rows= self.parse_train(allTickets)
+        self.printtable(rows)
+
+    def parse_train(self,allTickets):
+        rows = []
+        for ticket in allTickets:
+            trainlist = ticket.split('|')
+            trainrow=[]
+            # trainrow.extend(trainlist[3:6])
+            trainlist[4] = stations.getCityName(trainlist[4])
+            trainlist[5] = stations.getCityName(trainlist[5])
+            # print(trainlist[4],trainlist[5])
+            trainrow.append(trainlist[3])
+            trainrow.append(trainlist[4])
+            trainrow.append(trainlist[5])
+            trainrow.extend(trainlist[8:11])
+            trainrow.extend(trainlist[32:21:-1])
+            row = []
+            for x in trainrow:
+                x = x or "--"
+                row.append(x)
+
+            rows.append(row)
+        return rows
+    
+    def printtable(self,rows):
+        
+        info = "车次 出发站 到达站 出发时间 到达时间 历时 商务座 特等座 一等座 二等座 高级 软卧 动卧 硬卧 软座 硬座 无座"
+        info = info.split(" ")
+        x = PrettyTable(info)
+        for row in rows:
+            x.add_row(row)
+
+        print(x.get_string()) 
+        
+
+
+if __name__ == '__main__':
+    Tickets().printTrainInfo()
